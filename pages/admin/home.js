967adb84a8ebc897750ca7e670a80b3b5543a8c9
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-11-09 15:17:45 +0000

    added order page in admin

--- pages/admin/home.js
+++ pages/admin/home.js
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import AdminNav from "../../components/admin/AdminNav";
 import { useEffect } from "react";
+import Link from "next/link";
 
 function adminHome() {
-    
     const isLogged = useSelector(state=>state.kitchen.isLogged)
     const router = useRouter()
     useEffect(()=>{
@@ -16,7 +16,7 @@ function adminHome() {
             <AdminNav />
             <div className="max-w-full min-h-screen overflow-auto flex flex-row flex-wrap">
                 <button className="w-[10rem] h-[10rem] border-4 rounded-md border-sky-300 m-5 flex transition duration-150 ease-in-out hover:scale-110 active:scale-100 shadow-md shadow-black">
-                    <p className="m-auto">Orders</p>
+                    <p className="m-auto"><Link href={'/admin/order'}>Orders</Link></p>
                 </button>
             </div>
         </div>
---
+++ pages/admin/order.js
@@ -0,0 +1,14 @@
+import AdminNav from "../../components/admin/AdminNav";
+
+function order() {
+    return ( 
+        <>
+            <AdminNav />
+            <div className="max-w-full min-h-screen mx-1 mt-1 border-2 border-sky-400">
+            
+            </div>
+        </>
+     );
+}
+
+export default order;
