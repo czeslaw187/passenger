879d37175e6c550f44e879a4b4430c6868d685c4
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2023-02-09 20:34:25 +0000

    implementing card in index

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import salad from '../public/img/starter-salad.jpg'
 import vegan from '../public/img/main-vegan.jpg'
 import potatoe from '../public/img/starter-potatoes.jpg'
 import sides from '../public/img/starter-bruschetta.jpeg'
+import Spinner from "../components/spinner"
 
 import Navbar from "../components/Navbar"
 
@@ -25,7 +26,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <ul className="min-w-full max-h-full flex flex-row flex-wrap p-5">
+      <ul className="min-w-full max-h-full flex flex-row flex-wrap p-5">  
       {
         foodList.map((el,id)=>{
           return (
@@ -33,8 +34,8 @@ export default function Home() {
                 className="flex flex-col justify-center m-auto text-black transition duration-300 ease-in-out hover:scale-105 active:scale-100"
                 onClick={()=>{dispatch(setPage(el.category))}}>
               <Link href={'/menuId'} className="text-gray-500 no-underline">
-                <div className="w-[20rem] h-[20rem]  border-2 border-slate-600 relative hover:border-4 hover:border-sky-500">
-                  <Image src={el.item} fill alt="starter" />
+                <div className="w-[10rem] h-[10rem]  border-2 border-slate-600 relative hover:border-4 hover:border-sky-500">
+                  <Image src={el.item} fill alt="starter"/>
                 </div>
                 <p className="text-2xl mt-1 text-center font-serif">{el.category}</p>
               </Link>
